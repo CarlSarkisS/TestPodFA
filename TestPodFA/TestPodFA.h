2e96bc62dad797e50df3d8ec7947b708fa10c358
Carl SARKIS <carl.sarkis@non.se.com>
2021-02-22 14:12:05 +0100

    Initial Commit

---
+++ TestPodFA/TestPodFA.h
@@ -0,0 +1,18 @@
+//
+//  TestPodFA.h
+//  TestPodFA
+//
+//  Created by Carl Sarkis on 22/02/2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TestPodFA.
+FOUNDATION_EXPORT double TestPodFAVersionNumber;
+
+//! Project version string for TestPodFA.
+FOUNDATION_EXPORT const unsigned char TestPodFAVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TestPodFA/PublicHeader.h>
+
+
